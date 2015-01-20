7a1d1a606b545bd58bdd24c74046f3cb4f9256e2
mattlucian <matt.lucian@gmail.com>
2015-01-20 14:46:15 -0500

    Updates "About" page, adds Drink controller with stub pages, updates the site map.

---
+++ Vocatus/Vocatus/Controllers/DrinkController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vocatus.Controllers
+{
+    public class DrinkController : Controller
+    {
+        //
+        // GET: /Drink/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyDrinks()
+        {
+            return View();
+        }
+
+        public ActionResult Explore()
+        {
+            //TODO:
+                //pull list of all the alcoholic beverages
+                //pull list of all possible mixers
+                //insert these into a model to pass to the view
+                //
+            
+            return View();
+        }
+	}
+}
