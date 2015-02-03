576d68fc450285d5f220cfff7b4423329535e0fd
mattlucian <matt.lucian@gmail.com>
2015-02-03 16:18:34 -0500

    Adding missing models for keith build.

---
+++ Vocatus/Vocatus/Models/UserDrinkModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vocatus.Models
+{
+    public class UserDrinkModel 
+    {
+        public List<Ingredient> allPossibleIngredients = new List<Ingredient>();
+        public List<Ingredient> allCurrentIngredients = new List<Ingredient>();
+        public List<Cocktail> allPossibleCocktails = new List<Cocktail>();
+
+
+	}
+}
